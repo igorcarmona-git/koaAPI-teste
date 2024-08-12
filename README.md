31c773fa832f7fe1555af791bb7299983297d71a
igorcarmona-git <igorcarmonawork@gmail.com>
2024-08-12 00:32:14 -0300

    koa api with moch database teste

---
+++ README.md
@@ -0,0 +1,1 @@
+# koaAPI-teste --> npm i
---
+++ index.js
@@ -0,0 +1,51 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const koaBody = require('koa-body');
+const app = new Koa();
+const router = new Router();
+const PORT = 3050;
+
+// Mock database -->  é uma técnica usada em desenvolvimento e testes para simular o comportamento de um banco de dados real
+let users = [
+    {
+        id: 1,
+        name: 'Igor',
+        email: 'igorcarmona@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Michael',
+        email: 'michael@hotmail.com'
+    },
+    {
+        id: 3,
+        name: 'Pedro',
+        email: 'pedro@outlook.com'
+    }
+]
+
+router.get('/users/:id', ctx => {
+    if (!users[ctx.params.id - 1]) {
+        return ctx.throw(404, 'Usuário não encontrado');
+    }
+
+    ctx.status = 200;
+    ctx.body = users[ctx.params.id - 1];
+});
+
+router.post('/users/:id', ctx => {
+    if (!users[ctx.params.id - 1]) {
+        return ctx.throw(404, 'Usuário não encontrado');
+    }
+
+    ctx.status = 200;
+    ctx.body = Object.assign(users[ctx.params.id - 1], ctx.request.body);
+});
+
+app.use(koaBody.koaBody());
+app.use(router.allowedMethods())
+app.use(router.routes())
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
